0da0038f9ea7c126c96e26c92b65e09d5da2652e
shanesyy <shanesyy@woobo.io>
2019-07-10 17:52:00 -0700

    tried to config ssl

--- server/translate_plugin_server/app.py
+++ server/translate_plugin_server/app.py
@@ -9,11 +9,14 @@ import web.web_utils
 
 def main(port):
     app = web.web_utils.get_web_app()
-    http_server = tornado.httpserver.HTTPServer(app)
+    http_server = tornado.httpserver.HTTPServer(app, ssl_options={
+        "certfile": "/var/certificate/fullchain.pem",
+        "keyfile": "/var/certificate/privkey.pem",
+    })
     http_server.listen(port)
    
     tornado.ioloop.IOLoop.current().start()
 
 if __name__ == '__main__':
     print("start the service")
-    main(port=12000)
+    main(port=443)

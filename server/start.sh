636cc0284146a6d1b7ab9b258495f62999e085b3
shanesyy <shanesyy@woobo.io>
2019-07-10 17:56:36 -0700

    updated the start shell

--- server/start.sh
+++ server/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -itd -p 12000:12000 --name translate_server --network redis_default translate_plugin
+docker run -itd -p 443:443 --name translate_server --network redis_default translate_plugin
